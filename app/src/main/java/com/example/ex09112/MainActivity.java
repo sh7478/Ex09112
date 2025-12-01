050c894c7e238c56f8d8066e93e15006c4c96871
sh7478 <sh7478@bs.amalnet.k12.il>
2025-12-01 20:15:07 +0200

    Started the java program

--- app/src/main/java/com/example/ex09112/MainActivity.java
+++ app/src/main/java/com/example/ex09112/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.ex09112;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,15 +15,68 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etNum1, etNum2;
+    TextView solution;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNum1 = findViewById(R.id.etNum1);
+        etNum2 = findViewById(R.id.etNum2);
+        solution = findViewById(R.id.solution);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.menuAdd)
+        {
+            sumNums();
+        }
+        else if(id == R.id.menuSub)
+        {
+            subNums();
+        }
+        else if(id == R.id.menuMul)
+        {
+            mulNums();
+        }
+        else if(id == R.id.menuDiv)
+        {
+            divNums();
+        }
+        else if(id == R.id.menuClr)
+        {
+            clearAll();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sumNums() {
+        if(!etNum1.getText().equals(""))
+        {
+            if(!etNum2.getText().equals(""))
+            {
+                long num1 = Integer.parseInt(etNum1.getText().toString());
+                long num2 = Integer.parseInt(etNum2.getText().toString());
+                solution.setText("" + (num1 + num2));
+            }
+            else
+            {
+                //TODO: add an error output to solution
+            }
+        }
+        else
+        {
+            //TODO: add an error output to solution
+        }
+
     }
 }
