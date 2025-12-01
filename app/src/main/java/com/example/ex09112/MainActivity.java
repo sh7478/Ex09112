8316e95faf1ca959b87eab9d602afc98385cb01c
sh7478 <sh7478@bs.amalnet.k12.il>
2025-12-01 19:59:41 +0200

    Added menu

--- app/src/main/java/com/example/ex09112/MainActivity.java
+++ app/src/main/java/com/example/ex09112/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
         return super.onCreateOptionsMenu(menu);
     }
 }
